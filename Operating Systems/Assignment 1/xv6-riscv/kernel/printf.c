45d7669930d8c9d79a85979c37ca2b6a04ebcb4e
noamatia <noamatia15@gmail.com>
2021-04-28 11:52:36 +0300

    Add files via upload

--- Operating Systems/Assignment 1/xv6-riscv/kernel/printf.c
+++ Operating Systems/Assignment 1/xv6-riscv/kernel/printf.c
@@ -1,134 +1,113 @@
-//
-// formatted console output -- printf, panic.
-//
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
 #include <stdarg.h>
 
-#include "types.h"
-#include "param.h"
-#include "spinlock.h"
-#include "sleeplock.h"
-#include "fs.h"
-#include "file.h"
-#include "memlayout.h"
-#include "riscv.h"
-#include "defs.h"
-#include "proc.h"
-
-volatile int panicked = 0;
-
-// lock to avoid interleaving concurrent printf's.
-static struct {
-  struct spinlock lock;
-  int locking;
-} pr;
-
-static char digits[] = "0123456789abcdef";
+static char digits[] = "0123456789ABCDEF";
 
 static void
-printint(int xx, int base, int sign)
+putc(int fd, char c)
+{
+  write(fd, &c, 1);
+}
+
+static void
+printint(int fd, int xx, int base, int sgn)
 {
   char buf[16];
-  int i;
+  int i, neg;
   uint x;
 
-  if(sign && (sign = xx < 0))
+  neg = 0;
+  if(sgn && xx < 0){
+    neg = 1;
     x = -xx;
-  else
+  } else {
     x = xx;
+  }
 
   i = 0;
-  do {
+  do{
     buf[i++] = digits[x % base];
-  } while((x /= base) != 0);
-
-  if(sign)
+  }while((x /= base) != 0);
+  if(neg)
     buf[i++] = '-';
 
   while(--i >= 0)
-    consputc(buf[i]);
+    putc(fd, buf[i]);
 }
 
 static void
-printptr(uint64 x)
-{
+printptr(int fd, uint64 x) {
   int i;
-  consputc('0');
-  consputc('x');
+  putc(fd, '0');
+  putc(fd, 'x');
   for (i = 0; i < (sizeof(uint64) * 2); i++, x <<= 4)
-    consputc(digits[x >> (sizeof(uint64) * 8 - 4)]);
+    putc(fd, digits[x >> (sizeof(uint64) * 8 - 4)]);
 }
 
-// Print to the console. only understands %d, %x, %p, %s.
+// Print to the given fd. Only understands %d, %x, %p, %s.
 void
-printf(char *fmt, ...)
+vprintf(int fd, const char *fmt, va_list ap)
 {
-  va_list ap;
-  int i, c, locking;
   char *s;
+  int c, i, state;
 
-  locking = pr.locking;
-  if(locking)
-    acquire(&pr.lock);
-
-  if (fmt == 0)
-    panic("null fmt");
-
-  va_start(ap, fmt);
-  for(i = 0; (c = fmt[i] & 0xff) != 0; i++){
-    if(c != '%'){
-      consputc(c);
-      continue;
-    }
-    c = fmt[++i] & 0xff;
-    if(c == 0)
-      break;
-    switch(c){
-    case 'd':
-      printint(va_arg(ap, int), 10, 1);
-      break;
-    case 'x':
-      printint(va_arg(ap, int), 16, 1);
-      break;
-    case 'p':
-      printptr(va_arg(ap, uint64));
-      break;
-    case 's':
-      if((s = va_arg(ap, char*)) == 0)
-        s = "(null)";
-      for(; *s; s++)
-        consputc(*s);
-      break;
-    case '%':
-      consputc('%');
-      break;
-    default:
-      // Print unknown % sequence to draw attention.
-      consputc('%');
-      consputc(c);
-      break;
+  state = 0;
+  for(i = 0; fmt[i]; i++){
+    c = fmt[i] & 0xff;
+    if(state == 0){
+      if(c == '%'){
+        state = '%';
+      } else {
+        putc(fd, c);
+      }
+    } else if(state == '%'){
+      if(c == 'd'){
+        printint(fd, va_arg(ap, int), 10, 1);
+      } else if(c == 'l') {
+        printint(fd, va_arg(ap, uint64), 10, 0);
+      } else if(c == 'x') {
+        printint(fd, va_arg(ap, int), 16, 0);
+      } else if(c == 'p') {
+        printptr(fd, va_arg(ap, uint64));
+      } else if(c == 's'){
+        s = va_arg(ap, char*);
+        if(s == 0)
+          s = "(null)";
+        while(*s != 0){
+          putc(fd, *s);
+          s++;
+        }
+      } else if(c == 'c'){
+        putc(fd, va_arg(ap, uint));
+      } else if(c == '%'){
+        putc(fd, c);
+      } else {
+        // Unknown % sequence.  Print it to draw attention.
+        putc(fd, '%');
+        putc(fd, c);
+      }
+      state = 0;
     }
   }
-
-  if(locking)
-    release(&pr.lock);
 }
 
 void
-panic(char *s)
+fprintf(int fd, const char *fmt, ...)
 {
-  pr.locking = 0;
-  printf("panic: ");
-  printf(s);
-  printf("\n");
-  panicked = 1; // freeze uart output from other CPUs
-  for(;;)
-    ;
+  va_list ap;
+
+  va_start(ap, fmt);
+  vprintf(fd, fmt, ap);
 }
 
 void
-printfinit(void)
+printf(const char *fmt, ...)
 {
-  initlock(&pr.lock, "pr");
-  pr.locking = 1;
+  va_list ap;
+
+  va_start(ap, fmt);
+  vprintf(1, fmt, ap);
 }
--- Operating Systems/Assignment 1/xv6-riscv/kernel/printf.d
+++ Operating Systems/Assignment 1/xv6-riscv/kernel/printf.d
@@ -1,5 +1,2 @@
-kernel/printf.o: kernel/printf.c \
- /usr/local/lib/gcc/riscv64-unknown-elf/10.2.0/include/stdarg.h \
- kernel/types.h kernel/param.h kernel/spinlock.h kernel/sleeplock.h \
- kernel/fs.h kernel/file.h kernel/memlayout.h kernel/riscv.h \
- kernel/defs.h kernel/proc.h
+user/printf.o: user/printf.c kernel/types.h kernel/stat.h user/user.h \
+ /usr/local/lib/gcc/riscv64-unknown-elf/10.2.0/include/stdarg.h
